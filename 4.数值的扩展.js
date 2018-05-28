bbbc31c68196524304b7e4cee3297e9a96cfea57
zhanghaiquan <zhqustb@163.com>
2018-05-28 23:12:48 +0800

    'update'

---
+++ 4.数值的扩展.js
@@ -0,0 +1,10 @@
+/**
+ * 目录
+ * 1. Number.isFinite(), Number.isNaN()
+ * 2. Number.parseInt(), Number.parseFloat()
+ * 3. Number.isInteger()
+ * 4. Number.EPSILON
+ * 5. 安全整数和 Number.isSafeInteger()
+ * 6. Math 对象的扩展
+ * 7. 指数运算符
+ */
